022e90822a0ba7a520d04b086e6ee2984991f119
carlosrbatista1980 <carlosrbatista1980@gmail.com>
2019-02-28 02:15:56 -0300

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace RemoveProtectionInterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "...";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.Location = new System.Drawing.Point(259, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 27);
+            this.label3.TabIndex = 3;
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(290, 56);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(999, 200);
+            this.MinimumSize = new System.Drawing.Size(249, 85);
+            this.Name = "Form1";
+            this.Text = "Remover Proteção";
+            this.ResumeLayout(false);
+            this.Load += new System.EventHandler(this.Form1_Load);
+        }
+
+        //void Form1_Load(object sender, System.EventArgs e)
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Security.Principal;
+
+namespace RemoveProtectionInterApp
+{
+    public partial class Form1 : Form
+    {
+        bool existeCadeado = false;
+        bool isAdm = false;
+        string processPath = "";        
+
+        public Form1()
+        {
+            InitializeComponent();
+            //CheckAdmin();            
+        }
+
+        public void Form1_Load(object obj, EventArgs args)
+        {
+            CheckAdmin();            
+        }
+
+
+        private void CheckAdmin()
+        {
+            WindowsIdentity login = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(login);
+            isAdm = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (isAdm)
+            {
+                CarregaTimer();
+            }
+            else
+            {
+                MessageBox.Show("Execute como administrador", "");
+                this.Close();
+            }
+        }
+
+        private void DestruirCadeado()
+        {
+            Process[] cadeado = Process.GetProcesses();
+
+            foreach (Process proc in cadeado)
+            {
+                if (proc.ProcessName.Contains("qubnfe") || proc.ProcessName.Contains("winwmc"))
+                {
+                    processPath = proc.MainModule.FileName;
+                    proc.Kill();
+                    proc.WaitForExit();                    
+                    if (processPath != "")
+                    {
+                        File.Delete(processPath);
+                    }
+                }                
+            }
+            
+            Thread.Sleep(1000);
+            CarregaTimer();
+        }
+
+        private void CarregaTimer()
+        {
+            timer1.Interval = 2000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Enabled = true;
+        }
+
+        private void DetectarCadeado()
+        {           
+
+            Process[] cadeado = Process.GetProcesses();
+            
+            foreach (Process proc in cadeado)
+            {
+                if (proc.ProcessName == "qubnfe" || proc.ProcessName == "winwmc")
+                {
+                    label1.Text = "Cadeado Detectado !! ";
+                    existeCadeado = true;
+                    label1.ForeColor = Color.Red;
+                    label3.Visible = true;
+                    this.Update();
+                    Thread.Sleep(1000);                    
+                    label1.Text = "Removendo...";
+                    label1.ForeColor = Color.Red;
+                    label3.Visible = true;
+                    this.Update();
+                    Thread.Sleep(1000);                    
+                }                
+            }
+
+            if (!existeCadeado)
+            {
+                label1.ForeColor = Color.Blue;
+                label1.Text = "O InterApp não está executando.";                
+                label3.Visible = true;                
+                this.Update();
+                //Thread.Sleep(1000);
+                this.Update();
+            }            
+        }        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {                        
+            label1.ForeColor = Color.Black;
+            label1.Text = "....";
+            label3.Visible = false;
+            this.Update();
+            Thread.Sleep(200);
+            DetectarCadeado();
+            if (existeCadeado)
+            {
+                DestruirCadeado();            
+            }            
+        }        
+    }
+}
